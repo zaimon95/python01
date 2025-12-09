c4eac6124059b31d603198b95404f312c2a46623
Simon La Grange <sla-gran@fu-r5-p1.s19.be>
2025-12-09 13:17:55 +0100

    ex2

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,14 @@
+def ft_garden_intro():
+    name: str = "Rose"
+    height: int = 25
+    age: int = 30
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {name}")
+    print(f"Height: {height}cm")
+    print(f"Age: {age} days")
+    print()
+    print("=== End of Program ===")
+
+
+if __name__ == "__main__":
+    ft_garden_intro()
---
+++ ex2/ft_plant_growth.py
@@ -0,0 +1,33 @@
+class Plant:
+
+    def __init__(self, name, height, days):
+        self.name = name
+        self.height = height
+        self.days = days
+
+    def grow(self):
+        self.height += 1
+
+    def age(self):
+        self.days += 1
+        self.grow()
+
+    def get_info(self):
+        return f"{self.name}: {self.height}cm, {self.days} days old"
+
+
+def ft_plant_growth():
+    p1 = Plant("Rose", 25, 30)
+    growth: int = 0
+    print("=== Day 1 ===")
+    print(p1.get_info())
+    for i in range(6):
+        growth += 1
+        p1.age()
+    print("=== Day 7 ===")
+    print(p1.get_info())
+    print(f"Growth this week: +{growth}cm")
+
+
+if __name__ == "__main__":
+    ft_plant_growth()
