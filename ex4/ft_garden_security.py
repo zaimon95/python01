eb8915df5a85ba7cd4e9d92909b66db8528f9264
Simon La Grange <sla-gran@fu-r4-p2.s19.be>
2025-12-12 12:49:42 +0100

    fix(ex4): rajout de docstrings et fin

--- ex4/ft_garden_security.py
+++ ex4/ft_garden_security.py
@@ -1,14 +1,69 @@
-class SecurePlant:
-    def __init__(self, name, height, age) -> None:
-        self.name = name
-        self.height = height
-        self.age = age
-    def set_height(self, height) -> None:
-        self.height = height
-    def set_age(self, age) -> None:
-        self.age = age
-    def get_height(self) -> int:
-        return self.height
-    def get_age(self) -> int:
-        return self.age
+#!/usr/bin/env python3
+"""A module that creates a plant in a secure way."""
 
+
+class SecurePlant:
+    """A secure plant where the attributes are protrected."""
+
+    def __init__(self, name) -> None:
+        """Construct a SecurePlant.
+
+        :param name:
+            The name of the plant. It'a a string.
+        """
+        self.name = name
+
+    def set_height(self, height) -> None:
+        """Setter for height.
+
+        :param height:
+            The height of the plant. it's a number and cannot be negative.
+        """
+        if (height <= 0):
+            print()
+            print(f"Invalid operation attempted: height {height}cm [REJECTED]")
+            print("Security: Negative height rejected\n")
+        else:
+            self.__height = height
+            print(f"Height updated: {height}cm [OK]")
+
+    def set_age(self, age) -> None:
+        """Setter for age.
+
+        :param age:
+            The age of the plant. it's a number and cannot be negative.
+        """
+        if (age <= 0):
+            print()
+            print(f"Invalid operation attempted: age {age} days [REJECTED]")
+            print("Security: Negative age rejected\n")
+        else:
+            self.__age = age
+            print(f"Age updated: {age} days [OK]")
+
+    def get_height(self) -> int:
+        """Getter for height.
+
+        :returns:
+            The height of the plant.
+        """
+        return self.__height
+
+    def get_age(self) -> int:
+        """Getter for age.
+
+        :returns:
+            The age of the plant
+        """
+        return self.__age
+
+
+if __name__ == "__main__":
+    print("=== Garden Security System ===")
+    rose = SecurePlant("Rose")
+    print(f"Plant created: {rose.name}")
+    rose.set_height(25)
+    rose.set_age(30)
+    rose.set_height(-5)
+    print(f"Current plant: {rose.name} ", end='')
+    print(f"({rose.get_height()}cm, {rose.get_age()} days)")
