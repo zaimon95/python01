49dad6d64cbd7c04fa1915f884482c5111a18126
Simon La Grange <sla-gran@fu-r4-p8.s19.be>
2026-02-23 13:49:14 +0100

    creation python02

--- ex6/ft_garden_analytics.py
+++ ex6/ft_garden_analytics.py
@@ -2,13 +2,32 @@
 
 
 class Plant:
+
     def __init__(self, name, height, age) -> None:
         self.name = name
         self.height = height
         self.age = age
 
 
+class FloweringPlant(Plant):
+
+    def __init__(self, name, height, age, color) -> None:
+        super().__init__(name, height, age)
+        self.color = color
+
+    def blooming() -> bool:
+        return True
+
+
+class PrizeFlower(FloweringPlant):
+
+    def __init__(self, name, height, age, color, prize) -> None:
+        super().__init__(name, height, age, color)
+        self.prize = prize
+
+
 class Garden:
+
     def __init__(self, plants: list[Plant]) -> None:
         self.plants = plants
 
@@ -16,12 +35,18 @@ class Garden:
 class GardenManager:
 
     @classmethod
-    def create_garden_network(cls, garden: Garden) -> None:
+    def create_garden_network(cls, garden: list[Garden]) -> None:
         cls.garden = garden
-
 
     class GardenStats:
 
         @staticmethod
         def calculate_stats() -> None:
             print("calculating stats...")
+
+
+if __name__ == "__main__":
+    rose = PrizeFlower("Rose", 15, 2, "red", 100)
+    sunflower = FloweringPlant("Sunflower", 50, 20, "yellow")
+    alice = Garden([rose, sunflower])
+    manager = GardenManager([alice])
